e8deda895a71509b95d357a61ed60dc24c1fdb85
Atharva Deshmukh <atharva3230@gmail.com>
2024-07-13 01:10:36 +0530

    Authentication and database done

--- noteforge/app/page.tsx
+++ noteforge/app/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
-    <p>Hello Notion Clone</p>
+    <div>
+      <Button className="bg-emerald-500 text-white">
+        Delete
+      </Button>
+    </div>
   );
 }
